fd15e8b139e6d002920dc64c0de6f7fff00a8c9b
CHE <worasornjunpuan@gmail.com>
2023-07-20 19:09:46 +0700

    Daily edit: 2023-07-20

--- commit.sh
+++ commit.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-
+filename="daily_commit.txt"
+if [ ! -f "$filename" ]; then
+  touch "$filename"
+fi
+nano "$filename"
 # Add all changes to the staging area
 git add .
 
 # Commit with a custom commit message
-git commit -m "commit from the commit.sh"
+git commit -m "Daily edit: $(date +%Y-%m-%d)"
 
 git push 
