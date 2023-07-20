d83b4dd4529481f842fb1241e1edaa312db7afaa
CHE <worasornjunpuan@gmail.com>
2023-07-20 19:25:35 +0700

    Daily edit: 2023-07-20

--- daily.sh
+++ daily.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-# Change to your project directory
-cd /path/to/daily-commits
+# Function to generate a random word
+generate_random_word() {
+  # List of words you want to use for randomness
+  words=("apple" "banana" "cherry" "date" "elderberry" "fig" "grape" "honeydew" "kiwi" "lemon")
 
-# Define the filename for the daily commit
+  # Get the total number of words in the array
+  total_words=${#words[@]}
+
+  # Generate a random index to pick a random word from the array
+  random_index=$((RANDOM % total_words))
+
+  # Get the random word
+  random_word=${words[random_index]}
+
+  echo "$random_word"
+}
+
 filename="daily_commit.txt"
 
-# Check if the file exists; if not, create it
-if [ ! -f "$filename" ]; then
-  touch "$filename"
-fi
+# Generate a random word
+random_word=$(generate_random_word)
 
-# Read the commit message from the text file
-read -r commit_message < "$filename"
+# Add the random word to the file
+echo "$random_word" > "$filename"
 
-# Add the file to the staging area
-git add "$filename"
+# Add all changes to the staging area
+git add .
 
-# Commit with the custom commit message
-git commit -m "$commit_message"
+# Commit with a custom commit message
+git commit -m "Daily edit: $(date +%Y-%m-%d)"
 
-# Push changes to the remote repository (e.g., origin)
-git push origin main
+# Push to the remote repository
+git push
